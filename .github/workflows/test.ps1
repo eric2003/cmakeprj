4a87b57e54b1c151fa1b7e8f21f177c6f5d4d6a1
eric <fantasy_2003_@hotmail.com>
2021-07-11 11:44:54 +0800

    modify var action 88

--- .github/workflows/test.ps1
+++ .github/workflows/test.ps1
@@ -4,18 +4,42 @@ function test() {
 	Write-Host "Hello, OneFLOW-CFD!"
 }
 
+function MyGetFileName( $filePath ) {
+	$file_name_complete = [System.IO.Path]::GetFileName("$filePath")
+	$file_name_complete
+	#Write-Host "fileNameFull :" $file_name_complete	
+}
+
+function MyDownloadFile( $fullFilePath ) {
+	$my_filename = MyGetFileName("$fullFilePath")
+	$my_location = Get-Location
+	$my_local_filename = "$my_location" + "/" + $my_filename
+	
+	$my_client = new-object System.Net.WebClient
+	$my_client.DownloadFile( $fullFilePath, $my_local_filename )	
+}
+
 function InstallMSMPI() {
     mkdir mpi_dir
     cd mpi_dir
     # install MPI SDK and Runtime
     Write-Host "Installing Microsoft MPI SDK..."
+	$msmpisdk_webfilename = 'https://download.microsoft.com/download/A/E/0/AE002626-9D9D-448D-8197-1EA510E297CE/msmpisdk.msi'
+	MyDownloadFile( $msmpisdk_webfilename )
+	
     #appveyor DownloadFile https://download.microsoft.com/download/A/E/0/AE002626-9D9D-448D-8197-1EA510E297CE/msmpisdk.msi
     #Start-Process -FilePath msiexec.exe -ArgumentList "/quiet /qn /i msmpisdk.msi" -Wait
-    #Write-Host "Microsoft MPI SDK installation complete"
-    #Write-Host "Installing Microsoft MPI Runtime..."
-    #appveyor DownloadFile https://download.microsoft.com/download/A/E/0/AE002626-9D9D-448D-8197-1EA510E297CE/msmpisetup.exe
+    Write-Host "Microsoft MPI SDK installation complete"
+    Write-Host "Installing Microsoft MPI Runtime..."
+	
+	$msmpisetup_webfilename = 'https://download.microsoft.com/download/A/E/0/AE002626-9D9D-448D-8197-1EA510E297CE/msmpisetup.exe'
+	MyDownloadFile( $msmpisetup_webfilename )
+	
     #Start-Process -FilePath MSMpiSetup.exe -ArgumentList -unattend -Wait
-    #Write-Host "Microsoft MPI Runtime installation complete..."
+    Write-Host "Microsoft MPI Runtime installation complete..."
+	
+	ls
+	
     #cd 
     #dir
     #c:
